8c1f8cbf63911ec3e66ee2f586f1a04dab99695f
jason <2906998016@qq.com>
2015-07-10 13:12:29 +0800

    test git

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+  def error_404
+    render file: "#{Rails.root}/public/404.html" #, status: 404, layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {sessions: "users/sessions"}
   root to: 'home#index'
+
+  if Rails.env.development?
+    match '*match',via: :all,to: 'pages#error_404'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
