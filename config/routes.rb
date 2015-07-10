ca12bdf9aa2564b42501ea9345f9c0a7ccd9be2b
huaxiukun <huaxiukun@yiyc.com>
2015-07-10 13:29:34 +0800

    img

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   if Rails.env.development?
-    match '*match',via: :all,to: 'pages#error_404'
+    match '*path', via: :all, to: 'pages#error_404'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
