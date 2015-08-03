feb117cfad477cccae47d407e88edb5ae8e8bba9
lmiogit <yl_mio@sina.com>
2015-08-03 11:51:11 +0800

    test

--- public/admin/js/admin-layout.js
+++ public/admin/js/admin-layout.js
@@ -38,7 +38,7 @@ $(function () {
     //时间组件合并
     var dateMerge = $('[data-date-merge]');
     if (dateMerge.length > 0) {
-        $.each(dateMerge,function(){
+        $.each(dateMerge, function () {
             var self = $(this);
             var date = self.find('input[data-date-merge-date]');
             var time = self.find('input[data-date-merge-time]');
@@ -46,13 +46,33 @@ $(function () {
             date.on('change', function () {
                 var d = date.val();
                 var t = time.val();
-                result.val(d+' '+t);
+                result.val(d + ' ' + t);
             });
             time.on('change', function () {
                 var d = date.val();
                 var t = time.val();
-                result.val(d+' '+t);
+                result.val(d + ' ' + t);
             });
+        })
+    }
+
+    //search select init
+    var searchControl = $('[data-search]');
+    if (searchControl.length > 0) {
+        $.each(searchControl, function () {
+            var self = $(this);
+            var target = self.attr('data-search');
+            var targetInput = self.find('[name="' + target + '"]');
+            var option = self.find('[data-search-option]');
+            option.on('click', function () {
+                var val = $(this).attr('data-search-option');
+                targetInput.val(val);
+                self.find('[data-search-text]').text($(this).text());
+            });
+            var initVal = targetInput.val();
+            if (initVal.length > 0) {
+                self.find('[data-search-text]').text(self.find('[data-search-option="' + initVal + '"]').text());
+            }
         })
     }
 });
