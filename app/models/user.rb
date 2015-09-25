7fb68d8e9a9bfb151e88ca59ffae8ba782dee6fb
huaxiukun <huaxiukun@yiyc.com>
2015-09-25 16:51:11 +0800

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
+  has_many :team_user_ships, foreign_key: :user_id
+  has_many :teams
+  has_many :creative_activities
+  has_many :likes, foreign_key: :user_id
+  has_one :user_profile, :dependent => :destroy
+  has_many :roles, through: :user_roles
+  has_many :user_roles
+  has_many :notifications
+
+  mount_uploader :avatar, AvatarUploader
+
+  # accepts_nested_attributes_for :user_profile, allow_destroy: true
+  #
+  # delegate :username, :gender, :birthday, :address, to: :user_profile, allow_nil: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,11 +25,17 @@ class User < ActiveRecord::Base
   # validates :username, length: {in: 4..20}
   validates :nickname, presence: true, uniqueness: true, length: {minimum: 3, maximum: 10}
   validate :validate_email_mobile
+  after_create :create_uuid
 
   def validate_email_mobile
     if email.blank? && mobile.blank?
       errors[:email] << '邮箱不能为空'
     end
+  end
+
+  def create_uuid
+    self.uuid = SecureRandom.uuid
+    self.save
   end
 
   attr_accessor :login
@@ -23,7 +44,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value OR lower(nickname) =  :value OR lower(mobile) =  :value", {:value => login.downcase}]).first
+      where(conditions).where(["lower(email) = :value OR lower(nickname) =  :value OR lower(mobile) =  :value", {:value => login.downcase}]).first
     else
       where(conditions).first
     end
