558abbe87a2997c2fb57b682f461439f56d6cb7d
huaxiukun <huaxiukun@yiyc.com>
2015-09-28 10:35:36 +0800

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :nickname, :password, :password_confirmation, :captcha, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :nickname, :mobile, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password, :captcha, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,25 +1,82 @@
 class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  def create
-    super
+# GET /resource/sign_in
+  def new
+    @resource = resource_class.new(sign_in_params)
+    self.resource = @resource
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
   end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+# POST /resource/sign_in
+  def create
+    if params[:user][:login].present?
+      user = User.where(['nickname = :value OR email = :value OR mobile = :value', {:value => resource_params[:login]}]).first
+      if user.present?
 
-  # protected
+        if session[:fail_times].present? && session[:fail_times] > 10
+          if captcha_valid? params[:captcha]
+            if user.valid_password?(params[:user][:password])
+              super
+              session[:fail_times] = 0
+            else
+              session[:fail_times] += 1
+              flash[:error] = '密码不正确'
+              @resource = resource_class.new(sign_in_params)
+              @resource.login = resource_params[:login]
+              render action: 'new'
+            end
+          else
+            session[:fail_times] += 1
+            flash[:error] = '校证码不正确'
+            @resource = resource_class.new
+            @resource.login = resource_params[:login]
+            redirect_to action: 'new'
+            # render action: 'new'
+          end
+        else
+          if user.valid_password?(params[:user][:password])
+            super
+            session[:fail_times] = 0
+          else
+            if session[:fail_times].present?
+              session[:fail_times] += 1
+            else
+              session[:fail_times] = 1
+            end
+            flash[:error] = '密码不正确'
+            @resource = resource_class.new(sign_in_params)
+            @resource.login = resource_params[:login]
+            render action: 'new'
+          end
+        end
+      else
+        flash[:error] = '账户名不存在'
+        @resource = resource_class.new(sign_in_params)
+        @resource.login = resource_params[:login]
+        render action: 'new'
+      end
+    else
+      flash[:error] = '请输入账户名'
+      @resource = resource_class.new(sign_in_params)
+      render action: 'new'
+    end
+    puts '0989'
+    puts session[:fail_times]
+    # super
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+# DELETE /resource/sign_out
+# def destroy
+#   super
+# end
+
+# protected
+
+# If you have extra params to permit, append them to the sanitizer.
+# def configure_sign_in_params
+#   devise_parameter_sanitizer.for(:sign_in) << :attribute
+# end
 end
