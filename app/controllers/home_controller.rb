c2a62fe79c13e34ee7e5e6b877b63d92e202bf30
hxker <273208426@qq.com>
2015-12-25 17:09:03 +0800

    discourse

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @competition = Competition.last
-    @events = Event.where(competition_id: @competition.id)
+    if @competition.present?
+      @events = Event.where(competition_id: @competition.id)
+    end
   end
 end
